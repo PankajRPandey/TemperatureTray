3985ecee62fcd65763ffc08a8d69424ed3765dc8
Pankaj P <54427553+PankHub@users.noreply.github.com>
2020-11-02 00:32:31 +0530

    Modifying jwindow x coordinate to eql click location-x on taskbar

--- src/com/tt/TrayUI.java
+++ src/com/tt/TrayUI.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Area;
 
 public class TrayUI {
   public static void main(String[] args)throws Exception {
@@ -43,31 +44,59 @@ public class TrayUI {
           //p.setBackground(Color.red);
           jWindow.setSize(200, 100);
 
-          Rectangle bounds = TestTaskIcon.getSafeScreenBounds(e.getPoint());
-          Point point = e.getPoint();
-
-          int x = point.x;
-          int y = point.y;
-          if (y < bounds.y) {
-            y = bounds.y;
-          } else if (y > bounds.y + bounds.height) {
-            y = bounds.y + bounds.height;
+//          Rectangle bounds = TestTaskIcon.getSafeScreenBounds(e.getPoint());
+//          Point point = e.getPoint();
+//
+//          int x = point.x;
+//          int y = point.y;
+//          if (y < bounds.y) {
+//            y = bounds.y;
+//          } else if (y > bounds.y + bounds.height) {
+//            y = bounds.y + bounds.height;
+//          }
+//          if (x < bounds.x) {
+//            x = bounds.x;
+//          } else if (x > bounds.x + bounds.width) {
+//            x = bounds.x + bounds.width;
+//          }
+//
+//          if (x + jWindow.getPreferredSize().width > bounds.x + bounds.width) {
+//            x = (bounds.x + bounds.width) - jWindow.getPreferredSize().width;
+//          }
+//          if (y + jWindow.getPreferredSize().height > bounds.y + bounds.height) {
+//            y = (bounds.y + bounds.height) - jWindow.getPreferredSize().height;
+//          }
+//          jWindow.setLocation(x, y);
+          Point point1 = e.getPoint();
+          System.out.println(point1);
+          Rectangle screen = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                  .getMaximumWindowBounds();
+          String os = System.getProperty("os.name");
+          if (os.contains("Windows")) {
+            jWindow.setLocation(/*screen.width - 200 - 6*/point1.x, screen.height - 100 - 6);
+          } else if (os.contains("Mac")) {
+            //jWindow.setLocation(screen.width - windowSize.width + 6, 6);
           }
-          if (x < bounds.x) {
-            x = bounds.x;
-          } else if (x > bounds.x + bounds.width) {
-            x = bounds.x + bounds.width;
-          }
-
-          if (x + jWindow.getPreferredSize().width > bounds.x + bounds.width) {
-            x = (bounds.x + bounds.width) - jWindow.getPreferredSize().width;
-          }
-          if (y + jWindow.getPreferredSize().height > bounds.y + bounds.height) {
-            y = (bounds.y + bounds.height) - jWindow.getPreferredSize().height;
-          }
-          jWindow.setLocation(x, y);
           jWindow.setVisible(true);
 
+          //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//          GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+//          Rectangle bounds1 = gd.getDefaultConfiguration().getBounds();
+//          Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(gd.getDefaultConfiguration());
+//
+//          Rectangle safeBounds = new Rectangle(bounds1);
+//          safeBounds.x += insets.left;
+//          safeBounds.y += insets.top;
+//          safeBounds.width -= (insets.left + insets.right);
+//          safeBounds.height -= (insets.top + insets.bottom);
+//
+//          System.out.println("Bounds = " + bounds1);
+//          System.out.println("SafeBounds = " + safeBounds);
+//
+//          Area area = new Area(bounds1);
+//          area.subtract(new Area(safeBounds));
+//          System.out.println("Area = " + area.getBounds());
+          //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         }
       }
