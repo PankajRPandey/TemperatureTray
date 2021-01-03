896589d27da3b885f8b9b3caca4d86296e725ade
Pankaj P <54427553+PankHub@users.noreply.github.com>
2021-01-04 00:56:55 +0530

    Added custom JPanel skeleton

---
+++ src/com/tt/TemperatureJPanel.java
@@ -0,0 +1,25 @@
+package com.tt;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TemperatureJPanel extends JPanel {
+    public TemperatureJPanel(LayoutManager layout) {
+        super(layout);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+    @Override
+    public void setForeground(Color fg) {
+        super.setForeground(fg);
+    }
+
+    @Override
+    public void setBackground(Color bg) {
+        super.setBackground(bg);
+    }
+}
