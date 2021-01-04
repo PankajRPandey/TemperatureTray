5956ca200a978989ec3508efd53a9faf8f1297db
Pankaj P <54427553+PankHub@users.noreply.github.com>
2021-01-05 00:16:53 +0530

    Modified TemperatureJPanel to match current JPanel design

--- src/com/tt/TemperatureJPanel.java
+++ src/com/tt/TemperatureJPanel.java
@@ -11,6 +11,15 @@ public class TemperatureJPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Dimension arcs = new Dimension(15, 15);
+        int width = getWidth();
+        int height = getHeight();
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        //Draws the rounded opaque panel
+        graphics.setColor(new Color(0.0f, 0.0f, 0.0f, 0.80f));
+        graphics.fillRoundRect(0, 0, width - 1, height - 1, arcs.width, arcs.height);//paint background
     }
 
     @Override
