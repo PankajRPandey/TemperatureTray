c4f8a9b216f44c08c68a8260ad4329c7405bd810
PankajRPandey <54427553+PankajRPandey@users.noreply.github.com>
2021-06-13 00:16:07 +0530

    Removed a comment

--- src/com/tt/TTMain.java
+++ src/com/tt/TTMain.java
@@ -187,7 +187,6 @@ public class TTMain {
                             public void windowLostFocus(WindowEvent e) {
                                     mainPanel.revalidate();
                                     sysPanel.revalidate();
-                                    //jWindow.dispose();
                                 jWindow.setVisible(false);
                             }
                         });
